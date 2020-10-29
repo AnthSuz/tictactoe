186cda52ec9935162f72eb577dfa5292d98895b0
AnthSuz <mil4@hotmail.fr>
2020-10-29 18:44:09 +0100

    add first css

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Square from './Square'
 
+const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    width: '250px',
+    height: '250px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate : 'repeat(3, 1fr) / repeat(3, 1fr)'
+}
+
 function Board({squares, onClick}) {
     return (
-        <>
+        <div style={style}>
         <Square value="1" onClick={() => onClick("Hello Word")} />
         <Square value="2" onClick={() => onClick("Hello Word")} />
         <Square value="3" onClick={() => onClick("Hello Word")} />
@@ -13,7 +23,7 @@ function Board({squares, onClick}) {
         <Square value="7" onClick={() => onClick("Hello Word")} />
         <Square value="8" onClick={() => onClick("Hello Word")} />
         <Square value="9" onClick={() => onClick("Hello Word")} />
-        </>
+        </div>
     )
 }
 
